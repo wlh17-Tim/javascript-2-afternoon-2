b531554edf2315d9cb51ea581134be92d85ce013
Timothy Barna <audioclimb@gmail.com>
2020-02-13 16:19:38 -0700

    first 5 complete

--- practice.js
+++ practice.js
@@ -17,7 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first (arr) {
+  return arr[0]
+}
 
 
 
@@ -32,7 +34,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last (arr) {
+  return arr[arr.length - 1];
+}
 
 
 
@@ -47,7 +51,10 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper (family){
+  for(let i = 0; i < family.length; i++)
+  alert(family[i])
+}
 
 
 
@@ -62,7 +69,10 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+function reversedLooper (letters){
+  for(let i = letters.length -1; i >= 0; i--)
+  alert(letters[i])
+}
 
 
 
@@ -77,7 +87,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+function evenFinder(nums){
+  let evenNums = [];
+  for(let i = 0; i < nums.length; i++){
+    if (nums[i]%2==0){
+      evenNums.push(nums[i])
+    }
+  }
+    return evenNums;
+}
 
 
 
